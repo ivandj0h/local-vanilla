575c887ec4fc84efff20d2ad9bc2d666613a400d
ivandi1980 <ivandi.djoh@gmail.com>
2020-06-27 18:15:29 +0700

    refactoring all the javascript code and change it into class mode

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 ## Localstorage VanillaJS
 
+### About 
+This Application was developed using ```vanilla Javascript``` and ```localstorage```
+
 ### Getting Started
-This is how to save to the localstorage using vanillaJS
+To getting started just clone using command ```git clone https://github.com/ivandi1980/local-vanilla.git```
+    
+    Note : if you're using VSCode, you can install ```live-server``` plugin to run this app smoothly
+
+
+### ScreenShoot
+Here are the Screenshoot of this application
+
+![Screen](img/screen-1.png "This is Screen captured")
--- js/app.js
+++ js/app.js
@@ -1,50 +1,134 @@
-const btnInsert = document.getElementById("btnInsert");
-const btnClear = document.getElementById("btnClear");
-const localStorageOutput = document.getElementById("localStorageOutput");
-
-const elem = `
-            <div class="alert alert-danger alert-dismissible fade show mt-3" role="alert" style="width: 32rem;">
-            <strong>Alert</strong> The Data with the same value already exist!.
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            </div>`;
-
-const showAlert = () =>  localStorageOutput.innerHTML = elem;
-btnClear.onclick = e => localStorage.clear(e);
-btnInsert.onclick = e => {
-    e.preventDefault()
-    
-    const Key    = document.getElementById("inKey").value;
-    const Val    = document.getElementById("inVal").value;
-
-    if(Key && Val) {
-        localStorage.setItem(Key, Val)
-        document.getElementById("inKey").value = ''
-        document.getElementById("inVal").value = ''
-
-        for(let i=0; i<localStorage.length; i++) {
-            const key = localStorage.key(i)
-            const value = localStorage.getItem(key)
-    
-            if(value){
-            localStorageOutput.innerHTML += `
-                <div class="card mt-2" style="width: 32rem;">
-                    <div class="card-header">
-                        <strong>${key}</strong>
-                        </div>
-                        <ul class="list-group list-group-flush">
-                        <li class="list-group-item">${value}</li>
-                    </ul>
-                </div>`;
-            }
-            else {
-                localStorageOutput.innerHTML = 'The Data with the same value already exist'
-            }
-        }       
+// Book Class: Represents a Book
+class Book {
+    constructor(fname, lname, phone) {
+      this.fname = fname;
+      this.lname = lname;
+      this.phone = phone;
     }
-    else {
-        showAlert()
+  }
+  
+  // UI Class: Handle UI Tasks
+  class UI {
+    static displayBooks() {
+      const books = Store.getBooks();
+  
+      books.forEach((book) => UI.addBookToList(book));
     }
-}
-
+  
+    static addBookToList(book) {
+      const list = document.querySelector('#book-list');
+  
+      const row = document.createElement('tr');
+  
+      row.innerHTML = `
+        <td>${book.fname}</td>
+        <td>${book.lname}</td>
+        <td>${book.phone}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+      `;
+  
+      list.appendChild(row);
+    }
+  
+    static deleteBook(el) {
+      if(el.classList.contains('delete')) {
+        el.parentElement.parentElement.remove();
+      }
+    }
+  
+    static showAlert(message, className) {
+      const div = document.createElement('div');
+      div.className = `alert alert-${className}`;
+      div.appendChild(document.createTextNode(message));
+      const container = document.querySelector('.container');
+      const form = document.querySelector('#book-form');
+      container.insertBefore(div, form);
+  
+      // Vanish in 3 seconds
+      setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+  
+    static clearFields() {
+      document.querySelector('#fname').value = '';
+      document.querySelector('#lname').value = '';
+      document.querySelector('#phone').value = '';
+    }
+  }
+  
+  // Store Class: Handles Storage
+  class Store {
+    static getBooks() {
+      let books;
+      if(localStorage.getItem('books') === null) {
+        books = [];
+      } else {
+        books = JSON.parse(localStorage.getItem('books'));
+      }
+  
+      return books;
+    }
+  
+    static addBook(book) {
+      const books = Store.getBooks();
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  
+    static removeBook(isbn) {
+      const books = Store.getBooks();
+  
+      books.forEach((book, index) => {
+        if(book.isbn === isbn) {
+          books.splice(index, 1);
+        }
+      });
+  
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  }
+  
+  // Event: Display Books
+  document.addEventListener('DOMContentLoaded', UI.displayBooks);
+  
+  // Event: Add a Book
+  document.querySelector('#book-form').addEventListener('submit', (e) => {
+    // Prevent actual submit
+    e.preventDefault();
+  
+    // Get form values
+    const fname = document.querySelector('#fname').value;
+    const lname = document.querySelector('#lname').value;
+    const phone = document.querySelector('#phone').value;
+  
+    // Validate
+    if(fname === '' || lname === '' || phone === '') {
+      UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+      // Instatiate book
+      const book = new Book(fname, lname, phone);
+  
+      // Add Book to UI
+      UI.addBookToList(book);
+  
+      // Add book to store
+      Store.addBook(book);
+  
+      // Show success message
+      UI.showAlert('Book Added', 'success');
+  
+      // Clear fields
+      UI.clearFields();
+    }
+  });
+  
+  // Event: Remove a Book
+  document.querySelector('#book-list').addEventListener('click', (e) => {
+    // Remove book from UI
+    UI.deleteBook(e.target);
+  
+    // Remove book from store
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+  
+    // Show success message
+    UI.showAlert('Book Removed', 'success');
+  });
